d9aa0153b98f3f690f11b068b091d1f981c2ebb4
mikepmerritt <mikepmerritt@gmail.com>
2023-02-10 03:42:08 -0500

    fixed divoid propellor rotation

--- Assets/Scripts/DivoidPropellor.cs
+++ Assets/Scripts/DivoidPropellor.cs
@@ -8,19 +8,22 @@ public class DivoidPropellor : MonoBehaviour
     public Vector3 propAxis; // forward axis of divoid
     public Quaternion propQuaternion;
     public float propSpeed = 1.0f;
+    public GameObject propellor;
     
     // set up variables to use later
     void Start()
     {
         propAngle = 0.0f;
         // navigate to main divoid parent to get axis
-        GameObject propBase = transform.parent.gameObject;
-        GameObject divoidBase = propBase.transform.parent.gameObject;
-        GameObject divoid = divoidBase.transform.parent.gameObject;
-        propAxis = divoid.transform.forward;
+        // GameObject propBase = transform.parent.gameObject;
+        // GameObject divoidBase = propBase.transform.parent.gameObject;
+        // GameObject divoid = divoidBase.transform.parent.gameObject;
+        // propAxis = divoid.transform.forward;
+        // use forward (z direction) as axis
+        propAxis = Vector3.forward;
         // set propellor location to initial state using quaternion
         propQuaternion = Quaternion.AngleAxis(propAngle, propAxis);
-        transform.rotation = propQuaternion;
+        propellor.transform.localRotation = propQuaternion;
     }
 
     // spin the propellor gradually
@@ -28,6 +31,6 @@ public class DivoidPropellor : MonoBehaviour
     {
         propAngle += propSpeed;
         propQuaternion = Quaternion.AngleAxis(propAngle, propAxis);
-        transform.rotation = propQuaternion;
+        propellor.transform.localRotation = propQuaternion;
     }
 }
